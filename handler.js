40033a472b2b97a1625aa6795616b1f983fe00ac
Achilles Moraites <achimoraites@yahoo.gr>
2018-12-10 17:58:06 +0200

    added error handling

--- handler.js
+++ handler.js
@@ -14,39 +14,44 @@ module.exports.batchInsert = async event => {
   // 4) user gets notified when proccessing ends (all data have been inserted in table)
   // ------------------------------------------------------------------------------------ //
 
- 
+  try {
+   
+    // params for a batch send
+    const params = (Entries) => ({
+      Entries,
+      QueueUrl: process.env.QUEUEURL /* required */
+    });
 
-  // params for a batch send
-  const params = (Entries) => ({
-    Entries,
-    QueueUrl: process.env.QUEUEURL /* required */
-  });
-
-  // generate elements
-  const numberOfBooks = 100;
-  let books = [];
-  const actions = [];
-  for (let i=0;i<numberOfBooks;i++) {
-    books.push( entry(i, {isbn: uuid.v4(),title: `title#${uuid.v1()}`}) );
-    if (i % 5 == 0) {
+    // generate elements
+    const numberOfBooks = 100;
+    let books = [];
+    const actions = [];
+    for (let i=0;i<numberOfBooks;i++) {
+      books.push( entry(i, {isbn: uuid.v4(),title: `title#${uuid.v1()}`}) );
+      if (i % 5 == 0) {
       // send batch
-      actions.push(sqs.sendMessageBatch(params(books)).promise());
-      // delete contents so far
-      books = [];
+        actions.push(sqs.sendMessageBatch(params(books)).promise());
+        // delete contents so far
+        books = [];
+      }
     }
+
+    await Promise.all(actions);
+
+
+    return {
+      statusCode: 200,
+      body: {
+        message: 'Items sent for processing successfully!'
+      },
+    };
+   
+  } catch (error) {
+    return {
+      statusCode: 400,
+      error
+    };
   }
 
-  await Promise.all(actions);
-
-
-
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Items sent for processing successfully!',
-      input: event,
-    }),
-  };
 
 };
