f6c41b532c97dab6b321f9eb3791f508715c2de6
Achilles Moraites <achimoraites@yahoo.gr>
2018-12-10 16:59:08 +0200

    defining requirements

--- handler.js
+++ handler.js
@@ -1,14 +1,19 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+module.exports.batchInsert = async event => {
+
+  // 1) Send a number of books to the sqs queue
+  // 2) queueProcessor pulls the data from the queue
+  // 3) the data are inserted gradually to avoid exceeding dynamodb limits
+  // 4) user gets notified when proccessing ends (all data have been inserted in table)
+
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Items sent for processing successfully!',
       input: event,
     }),
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
