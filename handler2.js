4e7b276d520bb4e6691cdf90affb3732d23a54bd
Achilles Moraites <achimoraites@yahoo.gr>
2018-12-12 17:05:25 +0200

    using batchWrite to put items

--- handler2.js
+++ handler2.js
@@ -1,8 +1,7 @@
 'use strict';
-const AWS = require('aws-sdk');
-const dynamoDC = new AWS.DynamoDB.DocumentClient({});
-// helper
+// helpers
 const { publish, deleteTopic } = require('./helpers/snsHelper');
+const { batchWrite } = require('./helpers/dynamodbHelper');
 
 // gets books from the queue and insert them in table
 // sends an email via sns to notify the user
@@ -13,19 +12,14 @@ module.exports.queueProcessor = async event => {
   // the data are inserted gradually to avoid exceeding dynamodb limits
   // ------------------------------------------------------------------------------------ //
 
-  const params = ({isbn, title}) => ({
-    TableName: process.env.TABLE_NAME,
-    Item: {
-      isbn,
-      title
-    }
-  }); 
-
+  let topic = null;
   try {
     // read records from the queue
     const { Records } = event;
+    const Books = [];
     const actions = [];
-    let topic = null;
+    
+
     Records.forEach(record => {
       const recordObj = JSON.parse(record.body);
       // if true we are done
@@ -34,18 +28,14 @@ module.exports.queueProcessor = async event => {
         topic = recordObj.topic;
         // send email to user
         actions.push(publish('Your books have been inserted!',topic));
-       
         return;
       }
-      // insert to dynamodb table
-      actions.push( dynamoDC.put(params(recordObj)).promise() );
+      Books.push(recordObj);
     });
 
+    actions.push(batchWrite(Books));
     await Promise.all(actions);
 
-    // remove topic if exists
-    if (topic) await deleteTopic(topic);
-    
     
 
     return {
@@ -56,11 +46,14 @@ module.exports.queueProcessor = async event => {
     };
    
   } catch (error) {
+    await publish(`Error : ${error}`,topic);
     return {
       statusCode: 400,
       error
     };
+  } finally {
+    // remove topic if exists
+    if (topic) await deleteTopic(topic);
   }
-
 
 };
---
+++ helpers/dynamodbHelper.js
@@ -0,0 +1,40 @@
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB;
+
+
+// LOCAL HELPERS
+
+// create put request item
+const putBookItem = ({isbn, title}) => ({
+  
+  PutRequest: {
+    Item: {
+      "isbn": {
+        S: isbn
+      }, 
+      "title": {
+        S: title
+      }
+    }
+  }
+  
+});
+
+// create items array
+const processBooks = (Books) => {
+  return Books.map(book => putBookItem(book));
+};
+
+const params = (Books) => ({
+  RequestItems: {
+    "Books": Books
+  }
+});
+
+module.exports = {
+
+  batchWrite(Books) {
+    return dynamoDB.batchWriteItem( params(processBooks(Books)) ).promise();
+  }
+
+};
