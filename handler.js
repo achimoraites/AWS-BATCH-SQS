cc38ffa855105b8f4383d5a7cde2084cad87db93
Achilles Moraites <achimoraites@yahoo.gr>
2018-12-12 10:37:08 +0200

    subscribing email

--- handler.js
+++ handler.js
@@ -1,4 +1,6 @@
 'use strict';
+const AWS = require('aws-sdk');
+const sns = new AWS.SNS();
 // helper
 const { sendBatchedMessages } = require('./helpers/sqsHelper');
 
@@ -8,9 +10,19 @@ module.exports.batchInsert = async () => {
   try {
     // NOTIFICATION FEATURE
     //    get user_email
+    const user_email = process.env.EMAIL;
     //    create a topic for user
-    //    subscribe user to topic
-  // ------------------------------------------------------------------------------------ //
+    // const topic = await sns.createTopic(params).promise();
+
+    // subscribe user
+    const params = {
+      Protocol: 'EMAIL', /* required */
+      TopicArn: process.env.TOPIC, /* required */
+      Endpoint: user_email
+    };
+    await sns.subscribe(params).promise();
+   
+    // ------------------------------------------------------------------------------------ //
 
     // send 100 books with a batch size of 5
     await sendBatchedMessages(100, 5);
